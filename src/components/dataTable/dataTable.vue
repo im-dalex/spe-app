e724404b1d22e6298e672893a093f9f257078b5f
Digno Alexander Familia <dalex2525@hotmail.com>
2020-11-16 12:04:11 -0400

    add empty message to datatable

--- src/components/dataTable/dataTable.vue
+++ src/components/dataTable/dataTable.vue
@@ -1,10 +1,15 @@
 <template>
     <div class="text-left">
         <b-button variant="success" @click="navigate(0)" class="mb-3">Agregar Registro</b-button>
-        <b-table striped hover :fields="fields" :items="items">
+        <b-table striped hover :fields="fields" :items="items" 
+            show-empty 
+            emptyText="No existen registros para mostrar.">
             <template v-slot:cell(actions)="data">
                 <b-button variant="warning" @click="navigate(data.item.id)">Editar</b-button> | 
                 <b-button variant="danger" @click="deleteItem(data.item.id)">Eliminar</b-button>
+            </template>
+            <template #empty="scope">
+                <h4>{{ scope.emptyText }}</h4>
             </template>
         </b-table>
     </div>
